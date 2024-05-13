f90b1a96df9db85771ea44462e830cc46a794f53
manju1910 <manjusubramaniam02@gmail.com>
2024-05-13 18:10:33 +0530

    choice

--- main.py
+++ main.py
@@ -1,92 +1,142 @@
 import pyodbc
-
+from Entity.movie import Movie
+from DAO.movie_service import MovieService
 server_name= "DESKTOP-0EUUQEO\\SQLEXPRESS"
 database_name = "HexawareMoviesDB"
-
+ 
+ 
 conn_str = (
     f"Driver={{SQL Server}};"
     f"Server={server_name};"
     f"Database={database_name};"
     f"Trusted_Connection=yes;"
 )
-
-print("Welcome to the movies app")
+ 
+ 
 print(conn_str)
-
 conn = pyodbc.connect(conn_str)
-cursor = conn.cursor()
+# cursor = conn.cursor()
+# cursor.execute("Select 1")
+# print("Database connection is successful 🎊")
+ 
+ 
+# Service - Function that talk to DB | Layer that interacts with DB
+ 
+ 
+# Encapsulation
 
+ 
+ 
+class DirectorService:
+    pass
+ 
+ 
+class ActorService:
+    pass
+ 
+ 
+# Entity / Model
+# Encapsulation - Movie data
 
-def read_movies():
-    cursor.execute("SELECT * FROM Movies")
-    movies = cursor.fetchall()
-    for movie in movies:
-        print(movie)
+ 
+ 
 
+ 
+ 
 
-def create_movie(title, year, director_id):
-    cursor.execute(
-        "INSERT INTO Movies (Title, Year, DirectorId) VALUES (?, ?, ?)",
-        (title, year, director_id)
-    )
-    conn.commit()  
-
-
-def delete_movie(movie_id):
-    try:
-        cursor.execute("DELETE FROM MovieActors WHERE MovieId = ?", (movie_id,))
-        cursor.execute("DELETE FROM Movies WHERE Id = ?", (movie_id,))
-        conn.commit()
-        print("Movie deleted successfully!")
-    except pyodbc.IntegrityError as e:
-        print("Error:", e)
-
-
-def update_movie(movie_id, title, year, director_id):
-    cursor.execute(
-        "UPDATE Movies SET Title = ?, Year = ?, DirectorId = ? WHERE MovieId = ?",
-        (title, year, director_id, movie_id)
-    )
-    conn.commit()
-    print("Movie updated successfully!")
-
-
-def menu():
-    print("\nMenu:")
-    print("1. Create a movie")
-    print("2. Update a movie")
-    print("3. Delete a movie")
-    print("4. Exit")
-
-
+ 
+ 
+# Main menu
+# 1. Movie Management
+# 2. Director Management
+# 3. Actor Management
+# 4. Exit
+ 
+ 
+# Task 3
+# Movie Management Menu
+# 1. Add a Movie
+# 2. View All Movies
+# 3. Update a Movie  (Task 4)
+# 4. Delete a Movie
+# 5. Back
+ 
+# C - Create
+# R - Read
+# U - Update
+# D - Delete
+ 
+class MainMenu:
+    def movie_menu(self):
+        movie_service = MovieService(conn)
+    
+        while True:
+            print(
+                """      
+            1. Add a Movie
+            2. View all Movies
+            3. Update a Movie  
+            4. Delete a Movie
+            5. Back to main menu
+                    """
+            )
+            choice = int(input("Please choose from above options: "))
+    
+            if choice == 1:
+                title = input("Please enter movie title: ")
+                year = int(input("Please enter movie year: "))
+                director_id = int(input("Please enter movie director's id: "))
+                new_movie = Movie(title, year, director_id)
+                movie_service.create_movie(new_movie)
+            elif choice == 2:
+                movie_service.read_movies()
+            if choice == 3:
+                movie_id = int(input("Please enter movie's id: "))
+                title = input("Please enter movie title: ")
+                year = int(input("Please enter movie year: "))
+                director_id = int(input("Please enter movie director's id: "))
+                updated_movie = Movie(title, year, director_id)
+                movie_service.update_movie(updated_movie, movie_id)
+            elif choice == 4:
+                movie_id = int(input("Please tell a movie id to delete: "))
+                movie_service.delete_movie(movie_id)
+            elif choice == 5:
+                break
+    
+    
+    def director_menu(self):
+        pass
+    
+    
+    def actor_menu(self):
+        pass
+ 
+ 
+# Task 5 - Keep it in loop
 if __name__ == "__main__":
+    print("Welcome to the movies app")
+    main_menu=MainMenu()
     while True:
-        menu()
-        choice = input("Enter your choice (1/2/3/4): ")
-
-        if choice == "1":
-            title = input("Enter the movie title: ")
-            year = input("Enter the release year: ")
-            director_id = input("Enter the director ID: ")
-            create_movie(title, year, director_id)
-            print("Movie added successfully!")
-
-        elif choice == "2":
-            read_movies()
-            movie_id_to_update = input("Enter the ID of the movie you want to update: ")
-            title = input("Enter the updated movie title: ")
-            year = input("Enter the updated release year: ")
-            director_id = input("Enter the updated director ID: ")
-            update_movie(movie_id_to_update, title, year, director_id)
-
-        elif choice == "3":
-            read_movies()
-            movie_id_to_delete = input("Enter the ID of the movie you want to delete: ")
-            delete_movie(movie_id_to_delete)
-
-        elif choice == "4":
-            print("Exiting the program. Goodbye!")
+        print(
+            """      
+            1. Movie Management
+            2. Director Management
+            3. Actor Management
+            4. Exit
+                """
+        )
+ 
+        choice = int(input("Please choose from above options: "))
+ 
+        if choice == 1:
+            main_menu.movie_menu()
+        elif choice == 2:
+            main_menu.director_menu()
+        elif choice == 3:
+            main_menu.actor_menu()
+        elif choice == 4:
             break
-
-        else:
-            print("Invalid choice. Please enter a valid option.")
+ 
+    # Clean up code
+    # cursor.close()
+    # conn.close()
